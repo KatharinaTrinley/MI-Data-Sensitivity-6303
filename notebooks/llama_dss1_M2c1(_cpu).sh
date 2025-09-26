5bcab1b48b1717ee2cc926e8fc71b6dad67992a6
Katharina Trinley <152901977+KatharinaTrinley@users.noreply.github.com>
2025-09-26 17:09:58 +0200

    Add files via upload

---
+++ notebooks/llama_dss1_M2c1(_cpu).sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+#!/bin/bash
+
+# System packages
+# apt update
+# apt install -y graphviz libgraphviz-dev git-lfs
+
+# # Clone repositories
+# git clone https://github.com/hannamw/EAP-positional.git
+# cd EAP-positional
+# git checkout tutorial
+# pip install -e .
+# cd ..
+
+
+# # Python packages
+# pip install plotly datasets jaxtyping better_abc fancy_einsum wandb einops==0.7.0 sentencepiece
+# pip install datasets transformers huggingface_hub pygraphviz cmapy
+# pip install numpy==1.26.4
+#pip install transformer_lens
+pip install numpy==1.23.5
+
+# Then run your Python script
+#python notebooks/llama_dss1_m2c1_cpu.py
